8cb5bfad3007af90269ad22d4fc64a7ff42f026d
light-yagami9716 <rafidhasansydney@gmail.com>
2025-03-15 17:09:01 +0600

    Test01

--- src/BusBooking/Seat.java
+++ src/BusBooking/Seat.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package BusBooking;
 
@@ -13,10 +13,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-
 /**
  *
  * @author HP
+ * 
+ * 
  */
 public class Seat extends javax.swing.JFrame {
 
